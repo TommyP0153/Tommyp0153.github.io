b4fad0dea615d9c896d0da37f80204cf0a55042e
TommyP0153 <77998299+TommyP0153@users.noreply.github.com>
2021-01-25 14:48:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//Checking if any buttons and which buttons are pressed
+document.querySelector("#factForm").fact1.addEventListener("click", fact1);
+document.querySelector("#factForm").fact2.addEventListener("click", fact2);
+document.querySelector("#factForm").fact3.addEventListener("click", fact3);
+document.querySelector("#factForm").fact4.addEventListener("click", fact4);
+document.querySelector("#factForm").fact5.addEventListener("click", fact5);
